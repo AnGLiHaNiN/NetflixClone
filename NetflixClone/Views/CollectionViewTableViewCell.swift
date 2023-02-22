2fc939e4e8cb4e834a42a5c4ed9ccf316dd09a21
AnGLiHaNiN <vaskin200333@gmail.com>
2023-02-23 01:09:58 +0300

    Add Coredata

--- NetflixClone/Views/CollectionViewTableViewCell.swift
+++ NetflixClone/Views/CollectionViewTableViewCell.swift
@@ -53,6 +53,19 @@ class CollectionViewTableViewCell: UITableViewCell {
             self?.collectionView.reloadData()
         }
     }
+    
+    private func downloadTitleAt(indexPath: IndexPath){
+        
+        
+        DataPersistenceManager.shared.downloadTitleWith(model: titles[indexPath.row]) { result in
+            switch result {
+            case .success():
+                NotificationCenter.default.post(name: NSNotification.Name("Downloaded"), object: nil)
+            case .failure(let error):
+                print(error.localizedDescription) 
+            }
+        }
+    }
 }
 
 
@@ -101,6 +114,34 @@ extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionV
     }
     
     
+    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemsAt indexPaths: [IndexPath], point: CGPoint) -> UIContextMenuConfiguration? {
+        let config = UIContextMenuConfiguration(
+            identifier: nil,
+            previewProvider: nil) {[weak self] _ in
+                let downloadAction = UIAction(title: "Download ", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
+                    if let indexPath = indexPaths.first {
+                        self?.downloadTitleAt(indexPath: indexPath)
+                    }
+                }
+                return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
+            }
+        return config
+    }
+    
+    
+//    func collectionView(_ collectionView: UICollectionView, contextMenuConfigurationForItemsAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+//        let config = UIContextMenuConfiguration(
+//            identifier: nil,
+//            previewProvider: nil) { _ in
+//                let downloadAction = UIAction(title: "Download ", subtitle: nil, image: nil, identifier: nil, discoverabilityTitle: nil, state: .off) { _ in
+//                    //self?.downloadTitleAt(indexPath: indexPath)
+//                }
+//                return UIMenu(title: "", image: nil, identifier: nil, options: .displayInline, children: [downloadAction])
+//            }
+//        return config
+//    }
+    
+    
     //Chat GPT
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
             if let collectionView = scrollView as? UICollectionView {
@@ -115,4 +156,7 @@ extension CollectionViewTableViewCell: UICollectionViewDataSource, UICollectionV
                 targetContentOffset.pointee = offset
             }
         }
+    
+    
+     
 }
